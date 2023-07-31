395f3d9fc282bceab78825dc52c90725b6e119cd
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-07-31 11:02:24 -0500

    finish lesson 53

--- Chapter 6/sandbox.js
+++ Chapter 6/sandbox.js
@@ -118,4 +118,20 @@ console.log(title.previousElementSibling);
 
 // Lesson 53
 
-const button = document.querySelector('button');
+// const button = document.querySelector("button");
+
+// button.addEventListener("click", () => {
+//   console.log("you clicked me");
+// });
+
+const items = document.querySelectorAll("li");
+
+items.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    // console.log("you clicked me");
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(item);
+    e.target.style.textDecoration = "line-through";
+  });
+});
