cadd11b0ab8e19f480cc632557b3126d7e553cde
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-07-31 10:11:33 -0500

    Chap 52 done

---
+++ Chapter 5/sandbox.js
@@ -0,0 +1,38 @@
+// object literals
+
+// const blogs = [
+//   { title: "why mac n cheese", likes: 50 },
+//   { title: "10 things I love", likes: 30 },
+// ];
+
+// console.log(blogs);
+
+/*
+let user = {
+  name: "John",
+  age: 36,
+  email: "john@example.com",
+  location: "usa",
+  blogs: [
+    { title: "why mac n cheese", likes: 50 },
+    { title: "10 things I love", likes: 30 },
+  ],
+  login() {
+    console.log("the user logged in");
+  },
+  logout() {
+    console.log("the user logged out");
+  },
+  logBlogs() {
+    console.log(this.blogs);
+    console.log("this user has written the following blogs:");
+    this.blogs.forEach((blog) => console.log(blog.title, blog.likes));
+  },
+};
+
+user.logBlogs();
+*/
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
---
+++ Chapter 6/sandbox.js
@@ -0,0 +1,114 @@
+// Lesson 46
+
+/*
+const para = document.querySelector("body > h1");
+
+console.log(para);
+
+const paras = document.querySelectorAll("p");
+
+const errors = document.querySelectorAll(".error");
+
+paras.forEach((para) => {
+  console.log(para);
+});
+
+console.log(errors);
+*/
+
+// Lesson 47
+
+/*
+// get an element by ID
+
+const title = document.getElementById("page-title");
+console.log(title);
+
+// get elements by their class name
+const errors = document.getElementsByClassName("error");
+console.log(errors);
+console.log(errors[0]);
+
+// get elements byy their tag name
+const paras = document.getElementsByTagName("p");
+console.log(paras);
+*/
+
+// Lesson 48
+
+/*
+
+const para = document.querySelector("p");
+
+// console.log(para.innerText);
+// para.innerText = "ninjas are awesome!";
+
+const paras = document.querySelectorAll("p");
+
+// paras.forEach((para) => {
+//   console.log(para.innerText);
+// });
+
+const content = document.querySelector(".content");
+
+// console.log(content.innerHTML);
+
+// content.innerHTML += "<h2>this is a new h2</h2>";
+// console.log(content);
+
+const people = ["mario", "luigi", "yoshi"];
+
+people.forEach((person) => {
+  content.innerHTML += `<p>${person}</p>`;
+});
+ */
+
+// Lesson 49
+/*
+const link = document.querySelector("a");
+
+console.log(link.getAttribute("href"));
+
+link.setAttribute("href", "https://oldschool.runescape.wiki/");
+link.innerText = "Oldschool RS Wiki";
+
+const mssg = document.querySelector("p");
+
+console.log(mssg.getAttribute("class"));
+mssg.setAttribute("class", "success");
+mssg.setAttribute("style", "color: green;");
+*/
+
+// Lesson 50
+
+/*
+const title = document.querySelector("h1");
+
+// title.setAttribute("style", "margin: 50px");
+
+console.log(title.style);
+console.log(title.style.color);
+
+title.style.margin = "50px";
+title.style.color = "crimson";
+title.style.fontSize = "60px";
+title.style.margin = "";
+*/
+
+// Lesson 56
+
+const article = document.querySelector("article");
+
+// console.log(article.children);
+// console.log(Array.from(article.children));
+
+// Array.from(article.children).forEach((child) => {
+//   child.classList.add("articleElement");
+// });
+
+const title = document.querySelector("h2");
+
+console.log(title.parentElement);
+console.log(title.parentElement.parentElement);
+console.log(title.nextElementSibling);
+console.log(title.previousElementSibling);
